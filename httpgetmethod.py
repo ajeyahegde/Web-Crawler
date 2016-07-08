a2c2f3c2ec0574c502b540e53682af4efb310c24
Ajeya SH <Ajeya SH>
2016-07-08 10:01:45 +0530

    extractUrls method completed and adds all the links present in that page

--- httpgetmethod.py
+++ httpgetmethod.py
@@ -1,13 +1,59 @@
 import urllib.request
 import re
-def HttpGet(url):
-    s=urllib.request.urlopen(url).read()
+
+#Function to get details of a page
+def httpGet(url):
+    #way of calling http GET method in python,html form is stored in s
+    htmlContent=urllib.request.urlopen(url).read()                      
     global html
-    html=str(s)
-    #print(s)
-def ExtractUrls():
-    links = re.findall('"((http|ftp)s?://.*?)"', html)
-    for link in links:
+    #string form of s is stored in html
+    html=str(htmlContent)                                               
+    print(htmlContent)
+    
+
+
+#Function to extract Urls    
+def extractUrls():
+    tempUrlList=[]
+    #mainUrlList - List to store all the links of the page
+    global mainUrlList
+    mainUrlList=[]
+    #linkForm-Regular expression pattern to find links present in page
+    linkForm="href=\".*?\""
+    foundedLinks=re.findall(linkForm, html)
+    #for loop that stores all links without some starting and ending string in list called tempUrlList
+    for link in foundedLinks:
+        link1 = link.split('"')
+        purelink=link1[1]
+        if(purelink.startswith(("#","javascript","/downloads")) or purelink.endswith((".js",".css",".xml",".png",".jpg",".gif",".tiff",".xhtml",".jpeg",".ico"))):
+            continue
+        tempUrlList.append(purelink)
+    #for loop for storing all the actual links obtained in mainUrlList     
+    for link in tempUrlList:
+        if(link.startswith(("http","https"))):
+            mainUrlList.append(link)
+            continue
+        elif(link.startswith("//")):
+            finalLink="http:"+link               #TODO:original protocol should be used
+            mainUrlList.append(finalLink)
+        else:
+            if(link.startswith("/")):
+                finalLink=Url+link
+            else:
+                finalLink=Url+"/"+link
+            mainUrlList.append(finalLink)
+    for link in mainUrlList:  
         print(link)
-HttpGet("http://www.wikipedia.com")
-ExtractUrls()
+        
+
+
+#Method which initiates crawling
+def main():
+    global Url
+    Url="http://www.python.org"
+    httpGet(Url)
+    extractUrls()
+
+    
+main()    
+    

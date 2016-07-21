9018ae078fff40aa1e7549bf3d5944358a41ff79
Ajeya SH <Ajeya SH>
2016-07-21 20:16:40 +0530

    isCrawlable funtion updated

--- isCrawlable.py
+++ isCrawlable.py
@@ -2,20 +2,24 @@ import urllib.request
 import re
 import datetime
 robotsFileContent=""
-#Crawled list contins tuples with first element domain name and second element previous crawled time
+#Crawled list contins tuples (domain name,last crawled time+crawled delay).
 global crawledList
 crawledList=[]
 
 
-#Takes Url and return name of robots.txt file
+#Takes Url and returns name of robots.txt file and domain name
 def getRobotsname(url):
-    pattern1="(http|https)://www.\w+.\w{3}$"
+    #pattern1 checks for regular expression for domain name
+    pattern1="(http|https)://\w+.\w+.([^/]*)+$"
+    #pattern2 is regular expression for getting URL of the form "http://www.example.com/"
     pattern2="((http|https)\://\w*\.?.+\.\w+/)"
+    #if url is of the form http://www.example.com then "/robots.txt" is added else "robots.txt" is added to www.example.com/
     if((re.match(pattern1,url)) == None):
         x=re.findall(pattern2,url)
+        print(x)
         domainName=x[0][0]
         url=x[0][0]+"robots.txt"
-        return url,domainName
+        return url,domainName[:-1]
     else:
         domainName=url
         url=url+"/robots.txt"
@@ -27,30 +31,61 @@ def getRobotsname(url):
 def getDisallowedList(robotsFileContent):
     listOfLinesOfRobots=robotsFileContent.split(r"\n")
     disallowedlist=[]
+    allowedlist=[]
     crawlDelay=0
     for i in listOfLinesOfRobots:
         if(i.startswith("Disallow")):
             j=i.split(":")
             if j[1] not in disallowedlist:
                 disallowedlist.append(j[1])
+        if(i.startswith("Allow")):
+            j=i.split(":")
+            if j[1] not in allowedlist:
+                allowedlist.append(j[1])
         if(i.startswith("Crawl-delay")):
             j=i.split(":")
             crawlDelay=j[1]
             crawlDelay=int(crawlDelay)
-    return disallowedlist,crawlDelay
+    return disallowedlist,allowedlist,crawlDelay
 
 
 
 #Checks if any of the disallowed links are present in current Url,if present returns True
-def isallowed(url,disallowedlist):
-    print(url)
+def isallowed(url,disallowedlist,allowedlist):
+    domainPattern="(http|https)://\w+.\w+.[^/]*/"
+    pattern1="(http|https)://\w+.\w+.([^/]*)+$"
+    if(re.match(pattern1,url)):
+        return True
+    x=re.search(domainPattern,url)
+    startingIndexofUri=x.end()-1
+    #string conatins URL of the present URL
+    string=url[startingIndexofUri:]
+    print("URI is:",string)
+    check=None
     for i in disallowedlist:
-        pattern=i
-        print(pattern)
+        pattern=i.strip()
+        if pattern == "":
+            continue
         if "*" in pattern:
             pattern=pattern.replace("*",".*")
-        if(re.search(pattern,url) != None):
-            return True
+        if "?" in pattern:
+            pattern=pattern.replace("?","\?.*")
+        #print(pattern)
+        if(re.match(pattern,string) != None):
+            check=False
+            break
+    for i in allowedlist:
+        pattern=i.strip()
+        if "*" in pattern:
+            pattern=pattern.replace("*",".*")
+        if "?" in pattern:
+            pattern=pattern.replace("?","\?.*")
+        #print(pattern)
+        if(re.match(pattern,string) != None):
+            check=True
+            break
+    return check
+
 
 
 
@@ -61,33 +96,40 @@ def checkCrawledList(domainName):
     print(domainName)
     for tuples in crawledList:
         if domainName in tuples[0]:
+            #current time contains current system time
             currenttime=datetime.datetime.now()
             print("currenttime",currenttime)
             if(currenttime < tuples[1]):
-                return True
+                return False
 
 
 
-#function that takes an Url and return True if it cannot be crawled 
+#function that takes an Url and return True if it can be crawled
 def isCrawlable(url):
     robotUrl,domainName=getRobotsname(url)
     s=urllib.request.urlopen(robotUrl).read()
     global robotsFileContent
+    #robotsFileContent is string that contains content of robots.txt
     robotsFileContent=str(s)
     print(robotsFileContent)
-    disallowedlist,crawlDelay=getDisallowedList(robotsFileContent)
-    #for i in disallowedlist:
-     #   print(i)
+    #disallowed list contains list of disallowed URIs
+    disallowedlist,allowedlist,crawlDelay=getDisallowedList(robotsFileContent)
+    print("Disallowed:")
+    for i in disallowedlist:
+        print(i)
+    print("Allowed:")
+    for i in allowedlist:
+        print(i)
     print("Crawl Delay:",crawlDelay)
-    x=isallowed(url,disallowedlist)
-    print(x)
-    if(x==True):
-        return True
-    x=checkCrawledList(domainName)
-    print(x)
-    if(x==True):
-        return True
-    return False
+    check=isallowed(url,disallowedlist,allowedlist)
+    print(check)
+    if(check==False):
+        return False,domainName,crawlDelay
+    check=checkCrawledList(domainName)
+    print(check)
+    if(check==False):
+        return False,domainName,crawlDelay
+    return True,domainName,crawlDelay
 
 
 p=50
@@ -98,6 +140,6 @@ print(t2)
 crawledList.append(("http://www.google.com",t2))
 crawledList.append(("http://www.facebook.com",t1))
 print(crawledList)
-url="http://www.google.com/ebooks/musica"
+url="http://play.google.com/books/css"
 print(isCrawlable(url))
-
+pattern1="(http|https)://\w+.\w+.[^/]*/"

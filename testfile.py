e5f6a79b76f6040ae9ac88924cb00dd4afa8ed07
Ajeya SH <Ajeya SH>
2016-07-31 09:59:32 +0530

    getRobotsname testfile

--- testfile.py
+++ testfile.py
@@ -5,29 +5,27 @@ import datetime
 #from test1 import getRobotsname
 class testing(unittest.TestCase):
     def teststring(self):
-        inputurl="www.google.com"
-        global  crawledDomainList
-        crawledDomainList=[]
-        crawledDomainList.append(("http://www.bitbucket.com",datetime.datetime.now()+datetime.timedelta(seconds=10)))
-        crawledDomainList.append(("http://www.google.com",datetime.datetime.now()+datetime.timedelta(seconds=5)))
-        crawledDomainList.append(("http://www.facebook.com",datetime.datetime.now()+datetime.timedelta(seconds=-5)))
-        output=checkCrawledList(inputurl)
-        print(crawledDomainList)
+        inputurl="http://www.google.com/ebooks/musica"
+        output=getRobotsname(inputurl)
         print(output)
-        self.assertFalse(output)
+        self.assertEqual(output,("http://www.google.com/robots.txt","http://www.google.com"))
 
-
-def checkCrawledList(domainName):
-    if domainName.endswith("/"):
-        domainName=domainName[:-1]
-    #print(domainName)
-    for tuples in crawledDomainList:
-        if domainName in tuples[0]:
-            #current time contains current system time
-            currenttime=datetime.datetime.now()
-            print("currenttime",currenttime)
-            if(currenttime < tuples[1]):
-                return False
+def getRobotsname(url):
+    #pattern1 checks for regular expression for domain name
+    urlPattern1="((http|https)://)?\w+.\w+.([^/]*)+$"
+    #pattern2 is regular expression for getting URL of the form "http://www.example.com/"
+    urlPattern2="(((http|https)\://)?\w*\.?.+\.\w+/)"
+    #if url is of the form http://www.example.com then "/robots.txt" is added else "robots.txt" is added to www.example.com/
+    if((re.match(urlPattern1,url)) == None):
+        x=re.findall(urlPattern2,url)
+        #print(x)
+        domainName=x[0][0]
+        url=x[0][0]+"robots.txt"
+        return url,domainName[:-1]
+    else:
+        domainName=url
+        url=url+"/robots.txt"
+        return url,domainName
 
 if __name__=="__main__":
     unittest.main()

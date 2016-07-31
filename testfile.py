3c96d9407cd782a9ff2eed2150bf64ca236e31ab
Ajeya SH <Ajeya SH>
2016-07-31 10:08:10 +0530

    getDisallowedList testfile

--- testfile.py
+++ testfile.py
@@ -5,27 +5,47 @@ import datetime
 #from test1 import getRobotsname
 class testing(unittest.TestCase):
     def teststring(self):
-        inputurl="http://www.google.com/ebooks/musica"
-        output=getRobotsname(inputurl)
+        inputurl="http://www.bitbucket.com/robots.txt"
+        s=urllib.request.urlopen(inputurl).read()
+        content=str(s)
+        output=getDisallowedList(content)
         print(output)
-        self.assertEqual(output,("http://www.google.com/robots.txt","http://www.google.com"))
+        self.assertGreaterEqual(len(output[0]),0)
+        self.assertGreaterEqual(len(output[1]),0)
+        self.assertGreaterEqual(output[2],0)
 
-def getRobotsname(url):
-    #pattern1 checks for regular expression for domain name
-    urlPattern1="((http|https)://)?\w+.\w+.([^/]*)+$"
-    #pattern2 is regular expression for getting URL of the form "http://www.example.com/"
-    urlPattern2="(((http|https)\://)?\w*\.?.+\.\w+/)"
-    #if url is of the form http://www.example.com then "/robots.txt" is added else "robots.txt" is added to www.example.com/
-    if((re.match(urlPattern1,url)) == None):
-        x=re.findall(urlPattern2,url)
-        #print(x)
-        domainName=x[0][0]
-        url=x[0][0]+"robots.txt"
-        return url,domainName[:-1]
-    else:
-        domainName=url
-        url=url+"/robots.txt"
-        return url,domainName
+
+def getDisallowedList(robotsFileContent):
+    listOfLinesOfRobots=robotsFileContent.split(r"\n")
+    disallowedlist=[]
+    allowedlist=[]
+    crawlDelay=0
+    #user=1 if User-agent = * and user=0 if otherwise
+    user=0
+    for i in listOfLinesOfRobots:
+        if("User-agent" in i):
+            j=i.split(":")
+            j[1]=j[1].strip()
+            #print(j)
+            if(j[1]=="*"):
+                user=1
+                continue
+            else:
+                user=0
+        if(i.startswith("Disallow") and user == 1):
+            j=i.split(":")
+            if j[1] not in disallowedlist:
+                disallowedlist.append(j[1])
+        if(i.startswith("Allow") and user == 1):
+            j=i.split(":")
+            if j[1] not in allowedlist:
+                allowedlist.append(j[1])
+        if(i.startswith("Crawl-delay")):
+            j=i.split(":")
+            crawlDelay=j[1]
+            crawlDelay=int(crawlDelay)
+    return disallowedlist,allowedlist,crawlDelay
+
 
 if __name__=="__main__":
     unittest.main()
